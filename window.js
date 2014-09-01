5373f8d25db6f7cebe26d3e92c918843e168ffde
Simon Yost <simonyost@gmail.com>
2014-09-01 13:51:14 -0500

    Trying links

--- window.js
+++ window.js
@@ -1,19 +1,11 @@
 window.onload = function() {
-  // document.querySelector('#greeting').innerText =
-  //   'Hello, World! It is ' + new Date();
-  //chrome.app.window.current().fullscreen();
-  var injectIt = function() {
-    console.log('foobar?');
-    // webview.executeScript({ file: "jsPrintSetup.js" });
-  }
-  //var webview = document.getElementById("test");
-  //webview.addEventListener("loadstop", injectIt);
-  // webview.onload();
-  //webview.executeScript({ code: "document.body.style.backgroundColor = 'red'" });
-  // webview.addEventListener('load', function(e) {
-  //   console.log("jsPrintSetup");
-  // });
+  // http://thejourney.org/?mobileapp
+  var webview = document.getElementById("kiosk");
   
+  function clickMe() {
+    alert('clickMe test');
+    webview.src('http://google.com');
+  }
 };
 
 
