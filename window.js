cd24a3c2667b6835cce62201fbd637f61bae80aa
Simon Yost <simonyost@gmail.com>
2014-09-01 13:06:06 -0500

    Copied & modified Kraig's starting point

---
+++ window.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  // document.querySelector('#greeting').innerText =
+  //   'Hello, World! It is ' + new Date();
+  //chrome.app.window.current().fullscreen();
+  var injectIt = function() {
+    console.log('foobar?');
+    // webview.executeScript({ file: "jsPrintSetup.js" });
+  }
+  //var webview = document.getElementById("test");
+  //webview.addEventListener("loadstop", injectIt);
+  // webview.onload();
+  //webview.executeScript({ code: "document.body.style.backgroundColor = 'red'" });
+  // webview.addEventListener('load', function(e) {
+  //   console.log("jsPrintSetup");
+  // });
+  
+};
+
+
+
