3795035a00fdacf2dd3229d8ffc612097ce5fe0e
Simon Yost <simonyost@gmail.com>
2014-09-09 11:44:18 -0500

    keyboard 1

--- window.js
+++ window.js
@@ -1,4 +1,12 @@
-$(document).ready(function() {
-	console.log("fullscreen goes here...")
-	chrome.app.window.current().fullscreen;
-});
+var appStart = function() {
+	// console.log("fullscreen goes here...")
+	// chrome.app.window.current().fullscreen;
+
+		
+	var keyboard = ChromeSetting.get(chrome.accessibilityFeatures.virtualKeyboard);
+	console.log("keyboard: " + keyboard);
+
+	ChromeSetting.set({chrome.accessibilityFeatures.virtualKeyboard: true});
+}
+
+$(document).ready(appStart);
