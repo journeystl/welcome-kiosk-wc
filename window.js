d3d09913ddee5219222818460fc2081195a52622
Simon Yost <simonyost@gmail.com>
2014-09-09 13:12:06 -0500

    keyboard 2

--- window.js
+++ window.js
@@ -1,12 +1,6 @@
-var appStart = function() {
-	// console.log("fullscreen goes here...")
-	// chrome.app.window.current().fullscreen;
+var appStart = function appStart() {
 
-		
-	var keyboard = ChromeSetting.get(chrome.accessibilityFeatures.virtualKeyboard);
-	console.log("keyboard: " + keyboard);
-
-	ChromeSetting.set({chrome.accessibilityFeatures.virtualKeyboard: true});
+    ChromeSetting.set({chrome.accessibilityFeatures.virtualKeyboard: true});
 }
 
 $(document).ready(appStart);
