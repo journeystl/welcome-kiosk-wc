2972283093a5e21831ba52604f90113c797828f9
Kraig Hufstedler <kraigory@gmail.com>
2014-09-09 19:29:22 -0500

    copying

--- background.js
+++ background.js
@@ -17,5 +17,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
 
   console.log('background.js');
   // chrome.accessibilityFeatures.virtualKeyboard.set(true);
-  chrome.accessibilityFeatures.virtualKeyboard.get({'incognito': false}, function(details) {console.log(JSON.stringify(details))};);
+  chrome.accessibilityFeatures.virtualKeyboard.get(
+          {'incognito': false},
+          function(details) {console.log(JSON.stringify(details));});
 });
