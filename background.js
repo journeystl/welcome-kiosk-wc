ba72e248953a0217a31e20825f4442e211994b7e
Simon Yost <simonyost@gmail.com>
2014-09-12 14:57:05 -0500

    restored

--- background.js
+++ background.js
@@ -7,20 +7,11 @@
 chrome.app.runtime.onLaunched.addListener(function() {
 
   chrome.app.window.create(
-    "window.html",
-    {
-		id: "mainWindow",
-		state: "fullscreen",
-		frame: "none"
-    }
+    "window.html"
   );
 
-  console.log('background.js');
-  // chrome.accessibilityFeatures.virtualKeyboard.set(true);
-  chrome.accessibilityFeatures.virtualKeyboard.get(
-          {'incognito': false},
-          function(details) {console.log(JSON.stringify(details));});
+
   chrome.accessibilityFeatures.virtualKeyboard.set(
-          {value: true, scope: 'regular'},
-          function() {console.log('WORKED!');});
+          {value: true, scope: 'regular'}
+    );
 });
